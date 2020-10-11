b4ce79db1d23e9ef55f4eaddb9cc3d92823f0ece
Julia <juliaccmoffitt@gmail.com>
2020-10-10 18:07:33 -0700

    removed Order_test.c

--- Order_test.c
+++
@@ -1,7 +0,0 @@
-#include <Order.h>
-#include <stdbool.h>
-
-struct Order first = Order.new("staples", 123);
-if (first.open == true) printf("Order is open. I will now close the order\n");
-first.close_order;
-if (first.open == false) printf("Order is closed\n");
